7973e1c08a17cbdf615645315159c6a21ae8ffc2
Damien Sedgwick <damiensedgwick@icloud.com>
2021-06-11 21:34:26 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Damien K. Sedgwick on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
