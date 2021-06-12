988a85fdc6f087a26690d3fe0ad15ca5a35d41d1
Damien Sedgwick <damiensedgwick@icloud.com>
2021-06-12 14:46:09 +0100

    Built out start button, fruit card and onboarding.

--- Fructus/Views/FruitCardView.swift
+++ Fructus/Views/FruitCardView.swift
@@ -9,12 +9,41 @@ import SwiftUI
 
 struct FruitCardView: View {
     
-    // MARK: - PROPERTIES
-    
-    // MARK: - BODY
+    @State private var isAnimating: Bool = false
     
     var body: some View {
-        Text("Blueberry")
+        ZStack {
+            VStack(spacing: 20) {
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 8, x: 8, y: 8)
+                    .scaleEffect(isAnimating ? 0.8 : 0.5)
+                
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 25), radius: 8, x: 8, y: 8)
+                
+                Text("Blueberris are sweet nutritous and wildy popular all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 480)
+                
+                StartButtonView()
+            }
+        }
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                isAnimating = true
+            }
+        }
+        .frame(minWidth: 0, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
     }
 }
 
---
+++ Fructus/Views/StartButtonView.swift
@@ -0,0 +1,36 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Damien K. Sedgwick on 12/06/2021.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Button(action: {
+            print("Exit the onboarding")
+        }) {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.5))
+        }
+        .accentColor(Color.white)
+        
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
