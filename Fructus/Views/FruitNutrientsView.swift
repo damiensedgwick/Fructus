bb0e618b57a18db8f4d760b6408eddc2ed8fc1db
Damien Sedgwick <damiensedgwick@icloud.com>
2021-06-16 20:03:07 +0100

    Added nutritional info view

---
+++ Fructus/Views/FruitNutrientsView.swift
@@ -0,0 +1,48 @@
+//
+//  FruitNutrientsView.swift
+//  Fructus
+//
+//  Created by Damien K. Sedgwick on 16/06/2021.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    
+    var fruit: Fruit
+    let nutrients: [String] = ["Energy", "Sugar", "Fat", "Protein", "Vitamins", "Minerals"]
+    
+    var body: some View {
+        GroupBox() {
+            DisclosureGroup("Nutritional value per 100g") {
+                ForEach(0 ..< nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 4)
+                    
+                    HStack {
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(Font.system(.body).bold())
+                        
+                        Spacer(minLength: 20)
+                        Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
+                    }
+                    .padding(.top, 5)
+                    .padding(.bottom, 5)
+                }
+            }
+        }
+    }
+}
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView(fruit: fruitsData[0])
+            .preferredColorScheme(.dark)
+            .previewLayout(.fixed(width: 375, height: 480))
+            .padding()
+    }
+}
